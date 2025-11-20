d4b2b07caf685df92d2ed4f55dbd89d9f53e6288
hulusiersinertas <hulusi.ersin.ertas@eskisehir.bel.tr>
2025-11-20 13:22:12 +0300

    Update route.js

--- modules/route.js
+++ modules/route.js
@@ -65,7 +65,7 @@ export async function drawRouteToTask(gorev, clickedButton) {
         // ... API istekleri aynı kalıyor ...
         const requestBody = { coordinates: [startPoint, endPoint] };
         
-        const response = await fetch('https://api.openrouteservice.org/v2/directions/driving-car', {
+        const response = await fetch('https://api.openrouteservice.org/v2/directions/driving-hgv', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -101,3 +101,4 @@ export async function drawRouteToTask(gorev, clickedButton) {
         }
     }
 }
+
