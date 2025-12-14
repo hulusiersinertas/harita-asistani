41f39e1b8e18a966d50b5a8aed61d08998918c27
hulusiersinertas <hulusi.ersin.ertas@eskisehir.bel.tr>
2025-12-14 15:15:39 +0300

    Update config.js

--- modules/config.js
+++ modules/config.js
@@ -1,7 +1,7 @@
 export const config = {
     googleApiKey: 'AIzaSyBgtFHotp01PD_MHOTqfFmYHmP6Zb-mFsY',
     spreadsheetId: '1OEfIZ4nuhG236chhiBibFUXMf2VR8ivBw_WXd4Zxkqc',
-    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbzVAJEPjY8PKe4_A1AuddcoJ_f28vAflxI8OQ9BGXqndpnbO3mkxeQUd_CTHJqEA593/exec',
+    appsScriptUrl: 'https://script.google.com/macros/s/AKfycbyo5P_FcnJzT1Z1HFWR2tbVh3CvcSp7TU0EryYJIv0rmAeD-gzpUenBd-stEJVDs8od/exec',
     openRouteServiceApiKey: 'eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6ImE5MTRjZTI4M2E1YTQzODdiZmFhYzFkMmEyNDVkYmRjIiwiaCI6Im11cm11cjY0In0=',
 
     // SÜTUN HARİTASI (GÜNCELLENDİ)
@@ -20,3 +20,4 @@ export const config = {
         NOT: 17          // R (Girilen Not)
     }
 };
+
